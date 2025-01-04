35a9c1f311dec053b160ade17b272001372f7a89
Matoxx01 <matiasbarrios2010@gmail.com>
2025-01-04 03:30:32 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DailyThink',
+  webDir: 'dist'
+};
+
+export default config;
